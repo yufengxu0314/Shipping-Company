c3c1fc4b0933b679392dc402a81346bb7f24250a
UBC Student <dx0314@students.cs.ubc.ca>
2021-03-27 16:06:11 -0700

    added more functions in databaseHandler

--- retailCenter/src/database/databaseHandler.java
+++ retailCenter/src/database/databaseHandler.java
@@ -86,17 +86,120 @@ public class databaseHandler {
             getRetailCenter();
             getShippingOrder();
             getStaff();
-//            getInsurance();
-//            getOffer();
-//            getReceivedBy();
-//            getSchedule();
-//            getCourrier();
-//            getPostman/Postwoman();
-//            getSortingCenter();
-//            getTransportation();
-//            getAssign();
+            getInsurance();
+            getOffer();
+            getReceivedBy();
+            getSchedule();
+            getCourier();
+            getPostmanPostwoman();
+            getSortingCenter();
+            getTransportation();
+            getAssign();
 
         }
+
+    //Queries: INSERT Operation
+    public void addCustomer(String PhoneNumber, String Name, String UserName, String Password, String Address) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?,?)");
+            ps.setString(1, PhoneNumber);
+            ps.setString(2, Name);
+            ps.setString(3, UserName);
+            ps.setString(4, Password);
+            ps.setString(5, Address);
+
+            ps.executeUpdate();
+            connection.commit();
+            ps.close();
+            getCustomer();
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+            rollbackConnection();
+        }
+    }
+
+
+    //Queries: INSERT Operation
+    public void addOrders(int TrackingID, String ContentType, String OrderDate,
+                          int Weight, String Size, String ShippingMethod, int Price) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?,?,?,?)");
+            ps.setInt(1, TrackingID);
+            ps.setString(2, ContentType);
+            ps.setString(3, OrderDate);
+            ps.setInt(4, Weight);
+            ps.setString(5, Size);
+            ps.setString(6, ShippingMethod);
+            ps.setInt(7, Price);
+            ps.executeUpdate();
+            connection.commit();
+            ps.close();
+            getCustomer();
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+            rollbackConnection();
+        }
+    }
+
+    //Queries: DELETE Operation
+    public void deleteOrder(int TrackingID) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("DELETE FROM ShippingOrder WHERE TrackingID = ?");
+            ps.setInt(1, TrackingID);
+
+            int rowCount = ps.executeUpdate();
+            if (rowCount == 0) {
+                System.out.println(WARNING_TAG + " Order " + TrackingID + " does not exist!");
+            }
+
+            connection.commit();
+
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection();
+        }
+    }
+
+    //Queries: UPDATE Operation
+    public void updateCustomer(String Name, String PhoneNumber, String Address) {
+        try {
+            getCustomer();
+            PreparedStatement ps = connection.prepareStatement("UPDATE Customer SET PhoneNumber = ?, Name = ? WHERE Address = ?");
+            ps.setString(1, PhoneNumber);
+            ps.setString(2, Name);
+            ps.setString(3, Address);
+
+            int rowCount = ps.executeUpdate();
+            if (rowCount == 0) {
+                System.out.println("ERROR" + " Customer with address " + Address + " does not exist!");
+            }
+            connection.commit();
+            ps.close();
+            getCustomer();
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+            rollbackConnection();
+        }
+    }
+
+
+
+    //Queries: SELECTION Operation
+    public void searchCustomer(String PhoneNumber, String Name, String UserName, String Password, String Address) throws exception {
+
+        ArrayList<Customer> result = new ArrayList<>();
+
+
+
+    }
+
+    //Queries: SELECTION Operation
+    public void searchTracking(int TrackingID) {
+        ArrayList<ShippingOrder> result = new ArrayList<>();
+
+    }
+
 
     public ArrayList<Customer> getCustomer() {
         ArrayList<Customer> customer = new ArrayList<>();
@@ -215,6 +318,177 @@ public class databaseHandler {
         return staff;
     }
 
+
+
+    public ArrayList<Insurance> getInsurance() {
+        ArrayList<Insurance> insurances = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Insurance");
+            while(rs.next()) {
+                Insurance i = new Insurance(rs.getInt("TrackingID"), rs.getInt("Amount"), rs.getString("SenderPhoneNumber"));
+                insurances.add(i);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(insurances);
+        return insurances;
+    }
+
+
+
+    public ArrayList<Offer> getOffer() {
+        ArrayList<Offer> offers = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Offer");
+            while(rs.next()) {
+                Offer o = new Offer(rs.getInt("BranchNumber"), rs.getInt("TrackingID"));
+                offers.add(o);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(offers);
+        return offers;
+    }
+
+
+    public ArrayList<ReceivedBy> getReceivedBy() {
+        ArrayList<ReceivedBy> receivedBy = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ReceivedBy");
+            while(rs.next()) {
+                ReceivedBy r = new ReceivedBy(rs.getInt("BranchNumber"), rs.getString("ReceiveTime"), rs.getString("PhoneNumber"));
+                receivedBy.add(r);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(receivedBy);
+        return receivedBy;
+    }
+
+
+    public ArrayList<Schedule> getSchedule() {
+        ArrayList<Schedule> schedules = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Schedule");
+            while(rs.next()) {
+                    Schedule s = new Schedule(rs.getInt("BranchNumber"), rs.getInt("TrackingID"));
+                schedules.add(s);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(schedules);
+        return schedules;
+    }
+
+    public ArrayList<Courier> getCourier() {
+        ArrayList<Courier> couriers = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Courier");
+            while(rs.next()) {
+                Courier c = new Courier(rs.getInt("BranchNumber"), rs.getInt("StaffID"));
+                couriers.add(c);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(couriers);
+        return couriers;
+    }
+
+    public ArrayList<PostmanPostwoman> getPostmanPostwoman() {
+        ArrayList<PostmanPostwoman> postmanPostwomen = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PostmanPostwoman");
+            while(rs.next()) {
+                PostmanPostwoman p = new PostmanPostwoman(rs.getInt("BranchNumber"), rs.getInt("StaffID"), rs.getInt("PhoneNumber"));
+                postmanPostwomen.add(p);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(postmanPostwomen);
+        return postmanPostwomen;
+    }
+
+    public ArrayList<SortingCenter> getSortingCenter() {
+        ArrayList<SortingCenter> sortingCenters = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM SortingCenter");
+            while(rs.next()) {
+                SortingCenter s = new SortingCenter(rs.getInt("BranchNumber"), rs.getString("Address"));
+                sortingCenters.add(s);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(sortingCenters);
+        return sortingCenters;
+    }
+
+    public ArrayList<Transportation> getTransportation() {
+        ArrayList<Transportation> transportation = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Transportation");
+            while(rs.next()) {
+                Transportation t = new Transportation(rs.getInt("BranchNumber"), rs.getInt("TrackingID"), rs.getInt("StaffID"));
+                transportation.add(t);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(transportation);
+        return transportation;
+    }
+
+    public ArrayList<Assign> getAssign() {
+        ArrayList<Assign> assigns = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Assign");
+            while(rs.next()) {
+                Assign a = new Assign(rs.getInt("BranchNumber"), rs.getInt("TrackingID"), rs.getInt("StaffID"));
+                assigns.add(a);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(assigns);
+        return assigns;
+    }
+
+
+
+
     public ArrayList<Parcel> getParcel() {
         ArrayList<Parcel> parcel = new ArrayList<>();
         try {
@@ -232,101 +506,6 @@ public class databaseHandler {
         System.out.println(parcel);
         return parcel;
     }
-
-    //Queries: INSERT Operation
-    public void addCustomer(String PhoneNumber, String Name, String UserName, String Password, String Address) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?,?)");
-            ps.setString(1, PhoneNumber);
-            ps.setString(2, Name);
-            ps.setString(3, UserName);
-            ps.setString(4, Password);
-            ps.setString(5, Address);
-
-            ps.executeUpdate();
-            connection.commit();
-            ps.close();
-            getCustomer();
-        } catch (SQLException e) {
-            System.out.println("ERROR");
-            rollbackConnection();
-        }
-    }
-
-    //Queries: INSERT Operation
-    public void addOrders(int TrackingID, String ContentType, String OrderDate,
-                             int Weight, String Size, String ShippingMethod, int Price) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?,?,?,?)");
-            ps.setInt(1, TrackingID);
-            ps.setString(2, ContentType);
-            ps.setString(3, OrderDate);
-            ps.setInt(4, Weight);
-            ps.setString(5, Size);
-            ps.setString(6, ShippingMethod);
-            ps.setInt(7, Price);
-            ps.executeUpdate();
-            connection.commit();
-            ps.close();
-            getCustomer();
-        } catch (SQLException e) {
-            System.out.println("ERROR");
-            rollbackConnection();
-        }
-    }
-
-    //Queries: DELETE Operation
-    public void deleteOrder(int TrackingID) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("DELETE FROM ShippingOrder WHERE TrackingID = ?");
-            ps.setInt(1, TrackingID);
-
-            int rowCount = ps.executeUpdate();
-            if (rowCount == 0) {
-                System.out.println(WARNING_TAG + " Order " + TrackingID + " does not exist!");
-            }
-
-            connection.commit();
-
-            ps.close();
-        } catch (SQLException e) {
-            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
-            rollbackConnection();
-        }
-    }
-
-    //Queries: UPDATE Operation
-    public void updateCustomer(String Name, String PhoneNumber, String Address) {
-        try {
-            getCustomer();
-            PreparedStatement ps = connection.prepareStatement("UPDATE Customer SET PhoneNumber = ?, Name = ? WHERE Address = ?");
-            ps.setString(1, PhoneNumber);
-            ps.setString(2, Name);
-            ps.setString(3, Address);
-
-            int rowCount = ps.executeUpdate();
-            if (rowCount == 0) {
-                System.out.println("ERROR" + " Customer with address " + Address + " does not exist!");
-            }
-            connection.commit();
-            ps.close();
-            getCustomer();
-        } catch (SQLException e) {
-            System.out.println("ERROR");
-            rollbackConnection();
-        }
-    }
-
-
-
-    //
-    public void searchCustomer(String PhoneNumber, String Name, String UserName, String Password, String Address) throws exception {
-
-        ArrayList<Customer> result = new ArrayList<Customer>();
-
-
-    }
-
 
     private void dropBranchTableIfExists(){
             try {
